163578bbdae4f8d09dbe85fcba19507f5e28020e
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-02 16:06:47 +0100

    Add the STAT command skeleton

---
+++ src/fr/polytech/pop3/server/commands/STAT.java
@@ -0,0 +1,29 @@
+package fr.polytech.pop3.server.commands;
+
+import fr.polytech.pop3.server.commands.results.CommandResult;
+
+/**
+ * This class represents a STAT POP 3 command.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class STAT extends Command {
+
+	/**
+	 * The STAT command name.
+	 */
+	public static final String STAT_COMMAND_NAME = "STAT";
+
+	/**
+	 * Create a STAT POP 3 command.
+	 */
+	public STAT() {
+		super(STAT_COMMAND_NAME);
+	}
+
+	@Override
+	public CommandResult run(String[] parameters) {
+		return null;
+	}
+}
