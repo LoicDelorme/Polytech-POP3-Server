13abeead9a0b0720300cb2edd78de7af66c4d6a0
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-02 16:01:05 +0100

    Add the NOOP command skeleton

---
+++ src/fr/polytech/pop3/server/commands/NOOP.java
@@ -0,0 +1,29 @@
+package fr.polytech.pop3.server.commands;
+
+import fr.polytech.pop3.server.commands.results.CommandResult;
+
+/**
+ * This class represents a NOOP POP 3 command.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class NOOP extends Command {
+
+	/**
+	 * The NOOP command name.
+	 */
+	public static final String NOOP_COMMAND_NAME = "NOOP";
+
+	/**
+	 * Create a NOOP POP 3 command.
+	 */
+	public NOOP() {
+		super(NOOP_COMMAND_NAME);
+	}
+
+	@Override
+	public CommandResult run(String[] parameters) {
+		return null;
+	}
+}
