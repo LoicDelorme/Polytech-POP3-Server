9cc155dc3c5e69c286ddf064e7451ee6cfee5a12
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-02 16:03:09 +0100

    Add the RETR command skeleton

---
+++ src/fr/polytech/pop3/server/commands/RETR.java
@@ -0,0 +1,29 @@
+package fr.polytech.pop3.server.commands;
+
+import fr.polytech.pop3.server.commands.results.CommandResult;
+
+/**
+ * This class represents a RETR POP 3 command.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class RETR extends Command {
+
+	/**
+	 * The RETR command name.
+	 */
+	public static final String RETR_COMMAND_NAME = "RETR";
+
+	/**
+	 * Create a RETR POP 3 command.
+	 */
+	public RETR() {
+		super(RETR_COMMAND_NAME);
+	}
+
+	@Override
+	public CommandResult run(String[] parameters) {
+		return null;
+	}
+}
