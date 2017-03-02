cc1b6a110b92db3ad21c121d7da926c798cfd0e8
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-02 16:00:21 +0100

    Add the DELE command skeleton

---
+++ src/fr/polytech/pop3/server/commands/DELE.java
@@ -0,0 +1,29 @@
+package fr.polytech.pop3.server.commands;
+
+import fr.polytech.pop3.server.commands.results.CommandResult;
+
+/**
+ * This class represents a DELE POP 3 command.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class DELE extends Command {
+
+	/**
+	 * The DELE command name.
+	 */
+	public static final String DELE_COMMAND_NAME = "DELE";
+
+	/**
+	 * Create a DELE POP 3 command.
+	 */
+	public DELE() {
+		super(DELE_COMMAND_NAME);
+	}
+
+	@Override
+	public CommandResult run(String[] parameters) {
+		return null;
+	}
+}
