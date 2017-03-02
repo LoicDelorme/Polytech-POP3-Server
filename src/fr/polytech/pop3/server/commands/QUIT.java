10b0e0f690397b34e6a402969f652ff06a72cfbb
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-02 16:03:00 +0100

    Add the QUIT command skeleton

---
+++ src/fr/polytech/pop3/server/commands/QUIT.java
@@ -0,0 +1,29 @@
+package fr.polytech.pop3.server.commands;
+
+import fr.polytech.pop3.server.commands.results.CommandResult;
+
+/**
+ * This class represents a QUIT POP 3 command.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class QUIT extends Command {
+
+	/**
+	 * The QUIT command name.
+	 */
+	public static final String QUIT_COMMAND_NAME = "QUIT";
+
+	/**
+	 * Create a QUIT POP 3 command.
+	 */
+	public QUIT() {
+		super(QUIT_COMMAND_NAME);
+	}
+
+	@Override
+	public CommandResult run(String[] parameters) {
+		return null;
+	}
+}
