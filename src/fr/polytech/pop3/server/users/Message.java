5d817e1655c646410076950384c725d30a99a3c7
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-05 15:07:51 +0100

    Ehnance message entity

--- src/fr/polytech/pop3/server/users/Message.java
+++ src/fr/polytech/pop3/server/users/Message.java
@@ -9,27 +9,37 @@ package fr.polytech.pop3.server.users;
 public class Message {
 
 	/**
-	 * The UUID.
+	 * The UUID of the message.
 	 */
 	private final String UUID;
 
 	/**
-	 * The index.
+	 * The index of the message.
 	 */
 	private final int index;
 
 	/**
-	 * The size.
-	 */
-	private final int size;
-
-	/**
-	 * The content.
+	 * The content of the message (including both headers and body).
 	 */
 	private final String content;
 
 	/**
-	 * If it's marked.
+	 * The headers of the message.
+	 */
+	private final String headers;
+
+	/**
+	 * The body of the message.
+	 */
+	private final String body;
+
+	/**
+	 * The size of the message.
+	 */
+	private final int size;
+
+	/**
+	 * If the message is marked.
 	 */
 	private boolean isMarked;
 
@@ -37,56 +47,107 @@ public class Message {
 	 * Create a message.
 	 * 
 	 * @param UUID
-	 *            The UUID.
+	 *            The UUID of the message.
 	 * @param index
-	 *            The index.
-	 * @param size
-	 *            The size.
+	 *            The index of the message.
 	 * @param content
-	 *            The content.
+	 *            The content of the message.
 	 */
-	public Message(String UUID, int index, int size, String content) {
+	public Message(String UUID, int index, String content) {
 		this.UUID = UUID;
 		this.index = index;
-		this.size = size;
 		this.content = content;
+		this.headers = getHeaders(content);
+		this.body = getBody(content);
+		this.size = getSize(content);
 		this.isMarked = false;
 	}
 
 	/**
-	 * Get the UUID.
+	 * Get the headers from a message's content.
 	 * 
-	 * @return The UUID.
+	 * @param content
+	 *            The message's content.
+	 * @return The headers of the message.
+	 */
+	private String getHeaders(String content) {
+		return null;
+	}
+
+	/**
+	 * Get the body from a message's content.
+	 * 
+	 * @param content
+	 *            The message's content.
+	 * @return The body of the message.
+	 */
+	private String getBody(String content) {
+		return null;
+	}
+
+	/**
+	 * Get the size from a message's content.
+	 * 
+	 * @param content
+	 *            The message's content.
+	 * @return The size of the message.
+	 */
+	private int getSize(String content) {
+		return 0;
+	}
+
+	/**
+	 * Get the UUID of the message.
+	 * 
+	 * @return The UUID of the message.
 	 */
 	public String getUUID() {
 		return this.UUID;
 	}
 
 	/**
-	 * Get the index.
+	 * Get the index of the message.
 	 * 
-	 * @return The index.
+	 * @return The index of the message.
 	 */
 	public int getIndex() {
 		return this.index;
 	}
 
 	/**
-	 * Get the size.
+	 * Get the content of the message.
 	 * 
-	 * @return The size.
-	 */
-	public int getSize() {
-		return this.size;
-	}
-
-	/**
-	 * Get the content.
-	 * 
-	 * @return The content.
+	 * @return The content of the message.
 	 */
 	public String getContent() {
 		return this.content;
+	}
+
+	/**
+	 * Get the headers of the message.
+	 * 
+	 * @return The headers of the message.
+	 */
+	public String getHeaders() {
+		return this.headers;
+	}
+
+	/**
+	 * Get the body of the message.
+	 * 
+	 * @return The body of the message.
+	 */
+	public String getBody() {
+		return this.body;
+	}
+
+	/**
+	 * Get the size of the message.
+	 * 
+	 * @return The size of the message.
+	 */
+	public int getSize() {
+		return this.size;
 	}
 
 	/**
@@ -103,5 +164,12 @@ public class Message {
 	 */
 	public void mark() {
 		this.isMarked = true;
+	}
+
+	/**
+	 * Unmark the message.
+	 */
+	public void unmark() {
+		this.isMarked = false;
 	}
 }
