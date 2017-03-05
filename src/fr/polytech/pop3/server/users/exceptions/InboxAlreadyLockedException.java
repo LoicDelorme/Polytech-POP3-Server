fee3a47d062447458f6a9a6c10393d1b3ae9fd6e
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-05 16:20:55 +0100

    Add exceptions to handle potential server problems

---
+++ src/fr/polytech/pop3/server/users/exceptions/InboxAlreadyLockedException.java
@@ -0,0 +1,15 @@
+package fr.polytech.pop3.server.users.exceptions;
+
+/**
+ * This class represents a session already locked exception.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class InboxAlreadyLockedException extends Exception {
+
+	/**
+	 * The serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+}
---
+++ src/fr/polytech/pop3/server/users/exceptions/InvalidUsernameException.java
@@ -0,0 +1,15 @@
+package fr.polytech.pop3.server.users.exceptions;
+
+/**
+ * This class represents an invalid username exception.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class InvalidUsernameException extends Exception {
+
+	/**
+	 * The serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+}
